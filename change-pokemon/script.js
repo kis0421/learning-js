9f4d175cb4ae895074ccd4393bfb2494079e7a04
kis0421 <kig0421@gmail.com>
2022-04-17 22:55:26 +0900

    ui 그리도록

--- change-pokemon/script.js
+++ change-pokemon/script.js
@@ -2,3 +2,18 @@ const pokemonWrap = document.querySelector("#pokemonWrap");
 const inputRow = document.querySelector("#inputRow");
 const inputColumn = document.querySelector("#inputColumn");
 
+let width = 10;
+let height = 10;
+
+function render() {
+    inputRow.value = width;
+    inputColumn.value = height;
+    
+    for (let column = 0; column < height; column++){
+        for(let row = 0; row < width; row++){
+         
+        }
+    }
+}
+
+render()
