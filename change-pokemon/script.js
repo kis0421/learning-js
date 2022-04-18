0450bdf96b87f960d60e79a5a988df3acab313ca
김민규 <kig0421@gmail.com>
2022-04-18 14:06:50 +0900

    input keyup event로 일단 되게

--- change-pokemon/script.js
+++ change-pokemon/script.js
@@ -2,18 +2,22 @@ const pokemonWrap = document.querySelector("#pokemonWrap");
 const inputRow = document.querySelector("#inputRow");
 const inputColumn = document.querySelector("#inputColumn");
 
-let width = 10;
-let height = 10;
+let width, height
 
 function render() {
-    inputRow.value = width;
-    inputColumn.value = height;
-    
-    for (let column = 0; column < height; column++){
-        for(let row = 0; row < width; row++){
-         
-        }
+  width = inputRow.value;
+  height = inputColumn.value;
+
+  let targetElements = "";
+  for (let column = 0; column < height; column++) {
+    let columnChildElement = "";
+    for (let row = 0; row < width; row++) {
+      columnChildElement += `<img src="./image/pikachu.png" />`;
     }
+    targetElements += `<div>${columnChildElement}</div>`;
+  }
+
+  pokemonWrap.innerHTML = targetElements
 }
 
-render()
+render();
