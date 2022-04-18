63b08b1617cdb2554a1f258c6fe429bc33b5bded
kis0421 <kig0421@gmail.com>
2022-04-18 23:25:17 +0900

    changeInput 함수 분리

--- change-pokemon/script.js
+++ change-pokemon/script.js
@@ -6,13 +6,11 @@ function render() {
   pokemonWrap.innerHTML = `<div>${`<img src="./image/pikachu.png" />`.repeat(inputRow.value)}</div>`.repeat(inputColumn.value)
 }
 
-function changeInput(target, fn) {
-  if (target === "row") {
-    inputRow.value = fn(parseInt(inputRow.value, 10));
-  } else {
-    inputColumn.value = fn(parseInt(inputColumn.value, 10));
-  }
-  render()
+function setRow(fn) {
+  inputRow.value = fn(parseInt(inputRow.value, 10));
 }
 
+function setCoulumn(fn) {
+  inputColumn.value = fn(parseInt(inputColumn.value, 10));
+}
 render();
