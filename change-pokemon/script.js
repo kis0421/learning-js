c6ce24283655cf221ada7a7b63ac2e92f1dee3a4
김민규 <kig0421@gmail.com>
2022-04-18 14:40:28 +0900

    안쓰는 변수 제거

--- change-pokemon/script.js
+++ change-pokemon/script.js
@@ -2,23 +2,18 @@ const pokemonWrap = document.querySelector("#pokemonWrap");
 const inputRow = document.querySelector("#inputRow");
 const inputColumn = document.querySelector("#inputColumn");
 
-let width, height
-
 function render() {
-  width = inputRow.value;
-  height = inputColumn.value;
-
   let targetElements = "";
-  for (let column = 0; column < height; column++) {
+  for (let column = 0; column < inputColumn.value; column++) {
     let columnChildElement = "";
-    for (let row = 0; row < width; row++) {
+    for (let row = 0; row < inputRow.value; row++) {
       columnChildElement += `<img src="./image/pikachu.png" />`;
     }
     targetElements += `<div>${columnChildElement}</div>`;
   }
-
   pokemonWrap.innerHTML = targetElements
 }
+
 function changeInput(target, fn) {
   if (target === "row") {
     inputRow.value = fn(parseInt(inputRow.value, 10));
