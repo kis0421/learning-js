96434471cc8652ddf614282fa70d90be71fcd2a0
김민규 <kig0421@gmail.com>
2022-04-18 17:24:07 +0900

    부분업데이트

--- change-pokemon/script.js
+++ change-pokemon/script.js
@@ -3,15 +3,29 @@ const inputRow = document.querySelector("#inputRow");
 const inputColumn = document.querySelector("#inputColumn");
 
 function render() {
-  let targetElements = "";
-  for (let column = 0; column < inputColumn.value; column++) {
-    let columnChildElement = "";
-    for (let row = 0; row < inputRow.value; row++) {
-      columnChildElement += `<img src="./image/pikachu.png" />`;
+  const inputColumnValue = parseInt(inputColumn.value, 10);
+  const inputRowValue = parseInt(inputRow.value, 10);
+
+  if (inputColumnValue - pokemonWrap.childElementCount > 0) {
+    const textElements = `<div>${`<img src="./image/pikachu.png" />`.repeat(inputRowValue)}</div>`.repeat(inputColumnValue - pokemonWrap.childElementCount);
+    pokemonWrap.innerHTML += textElements;
+  } else if (inputColumnValue - pokemonWrap.childElementCount < 0) {
+    while (inputColumnValue - pokemonWrap.childElementCount < 0) {
+      pokemonWrap.removeChild(pokemonWrap.lastChild)
     }
-    targetElements += `<div>${columnChildElement}</div>`;
   }
-  pokemonWrap.innerHTML = targetElements
+
+  if (inputRowValue > document.querySelector("#pokemonWrap > div")?.childElementCount || 0) {
+    for (const element of document.querySelectorAll("#pokemonWrap > div")) {
+      element.insertAdjacentHTML("beforeend", `<img src="./image/pikachu.png" />`.repeat(inputRowValue - element.childElementCount || 0))
+    }
+  } else if (inputRowValue < document.querySelector("#pokemonWrap > div")?.childElementCount || 0) {
+    for (const element of document.querySelectorAll("#pokemonWrap > div")) {
+      while (element.childElementCount - inputRowValue > 0) {
+        element.removeChild(element.lastChild)
+      }
+    }
+  }
 }
 
 function changeInput(target, fn) {
